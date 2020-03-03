e8baafaa084b9691e9d222256a3263a0ca8e9f2a
jamesrenaudcode <61304413+jamesrenaudcode@users.noreply.github.com>
2020-03-03 16:34:56 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+
+
+export default FirebaseKeys = {
+    apiKey: "AIzaSyAp2y96Rjn-0ByroVe8DMTRSVe3Km2AK6w",
+    authDomain: "ghub-df94b.firebaseapp.com",
+    databaseURL: "https://ghub-df94b.firebaseio.com",
+    projectId: "ghub-df94b",
+    storageBucket: "ghub-df94b.appspot.com",
+    messagingSenderId: "782999207925",
+    appId: "1:782999207925:web:c94e475f8666f7686d6abc",
+    measurementId: "G-109F6XY6CB"
+  };
+ 
