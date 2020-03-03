30a58925a55c27e352685b5c61ff38349631f896
jamesrenaudcode <61304413+jamesrenaudcode@users.noreply.github.com>
2020-03-03 15:07:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,15 @@
 import { firebase } from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAp2y96Rjn-0ByroVe8DMTRSVe3Km2AK6w",
-  authDomain: "ghub-df94b.firebaseapp.com",
-  databaseURL: "https://ghub-df94b.firebaseio.com",
-  projectId: "ghub-df94b",
-  storageBucket: "ghub-df94b.appspot.com",
-  messagingSenderId: "782999207925",
-  appId: "1:782999207925:web:c94e475f8666f7686d6abc",
-  measurementId: "G-109F6XY6CB"
-};
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-export default FirebaseKeys
+export default FirebaseKeys = {
+        apiKey: "AIzaSyCEKfr8jeuPL4cmcCMbLYsVRq5tUjjMuk0",
+        authDomain: "grace-hub.firebaseapp.com",
+        databaseURL: "https://grace-hub.firebaseio.com",
+        projectId: "grace-hub",
+        storageBucket: "grace-hub.appspot.com",
+        messagingSenderId: "190767686892",
+        appId: "1:190767686892:web:1313e2c194a11c349271c9",
+        measurementId: "G-2YVP3JHMKZ"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
